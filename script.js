90e0a9f564410d212e4aa60da6c7db6a55138a6d
Kathiravan.m <159284720+Kathir073@users.noreply.github.com>
2025-01-12 17:42:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Menu Visibility
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('active');
+});
