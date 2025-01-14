ca2303f7a9511206c34b1ae214a4b8bea7a0686b
Kathiravan.m <159284720+Kathir073@users.noreply.github.com>
2025-01-14 11:53:49 +0530

    last commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle Menu Visibility
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('active');
+});
+document.getElementById("redirectButton").addEventListener("click", function () {
+  window.location.href = "form.html";
+});
+document.getElementById("redirectButton1").addEventListener("click", function () {
+  window.location.href = "form.html";
+});
