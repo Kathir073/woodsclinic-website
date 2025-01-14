5d993b4087e8fe44a8b9d8a4cc2fe3f596c958bd
Kathiravan.m <159284720+Kathir073@users.noreply.github.com>
2025-01-14 11:52:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Toggle Menu Visibility
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-  const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('active');
-});
